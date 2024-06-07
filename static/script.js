5a28bbd417fcef7bce3ad77688d31e6463313ead
jess <kittensealin@gmail.com>
2024-06-07 17:30:56 +0800

    multiple load test 2

--- static/script.js
+++ static/script.js
@@ -107,11 +107,14 @@ function load_attractions_more(page,keyword=''){
 					observer.observe(newTarget);
 				}
 			});	
-		}else{ observer.disconnect;}		
+		}else{ observer.disconnect();}		
 	}
 	let observer=new IntersectionObserver(callback,option);
 	let attbox=document.querySelector("div.att:nth-last-child(1)");
-	observer.observe(attbox);
+	if(attbox){
+		observer.observe(attbox);
+	}
+	
 }
 
 function query(keyword){
