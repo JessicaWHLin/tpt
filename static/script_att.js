bc3ae99a6b3482a07a0d04a9e4f207a459aab941
jess <kittensealin@gmail.com>
2024-06-13 23:04:42 +0800

    part 3-5

--- static/script_att.js
+++ static/script_att.js
@@ -17,13 +17,10 @@ changeObsever.addEventListener("change",(event)=>{
 
 //load attraction to sectionInfo
 let attractionId=localStorage.getItem("attractionId");
-// console.log("attractionId="+attractionId);
 url="/api/attraction/"+attractionId;
 fetch(url).then(e=>{
 	return e.json();
 }).then((data)=>{
-	// console.log(data.data);
-	// console.log(data.data.name);
 	let attName=document.querySelector(".h3_attName");
 	let attMrtCategory=document.querySelector(".text_category_mrt");
 	let info=document.querySelector(".info .detail");
@@ -34,8 +31,41 @@ fetch(url).then(e=>{
 	transport.textContent=data.data.transport;
 	attName.textContent=data.data.name;
 	attMrtCategory.textContent=data.data.category+" at "+data.data.mrt;
-	
-	}).catch(error=>console.error("Error:", error));
+	//slideshows-拿圖片
+	let picture_container=document.querySelector(".picture_current");
+	let circle_wrapper=document.querySelector(".circle_wrapper");
+	for(let i=0;i<data.data.images.length;i++){
+		let mySlide=document.createElement("div");
+		let img=document.createElement("img");
+		let circle=document.createElement("div");
+		circle.classList.add("circle");
+		mySlide.classList.add("mySlides");
+		mySlide.classList.add("fade");
+		img.src=data.data.images[i];
+		img.classList.add("slideshow");
+		mySlide.appendChild(img);
+		picture_container.appendChild(mySlide);
+		circle_wrapper.appendChild(circle);
+	}
+	//slideshow-動畫
+	let slidepage=1;
+	let current_slidepage=showSlides(slidepage);
+	let leftArrow=document.querySelector(".arrow_container_img.left");
+	let rightArrow=document.querySelector(".arrow_container_img.right");
+	leftArrow.addEventListener("click",()=>{
+		current_slidepage=plusSlides(-1,current_slidepage);
+	});
+	rightArrow.addEventListener("click",()=>{
+		current_slidepage=plusSlides(1,current_slidepage);
+	});
+	document.addEventListener('keydown', (event)=>{
+		if (event.key === "ArrowLeft") {
+			current_slidepage=plusSlides(-1,current_slidepage);
+		} else if (event.key === "ArrowRight") {
+			current_slidepage=plusSlides(1,current_slidepage);
+			}
+	});
+}).catch(error=>console.error("Error:", error));
 
 //回首頁
 let homePage=document.querySelector(".home");
@@ -45,3 +75,33 @@ homePage.addEventListener("click",()=>{
 	 location.href=url;
     }).catch(error=>console.error("Error:", error));
 });
+
+
+//函式區
+function showSlides(n){
+	let slides=document.querySelectorAll(".mySlides");
+	let circles=document.querySelectorAll(".circle");
+	let slidePage=1;
+	if(n>slides.length){
+		slidePage=1;
+	}
+	else if(n<1){
+		slidePage=slides.length;
+	}
+	else{
+		slidePage=n;
+	}
+	for(let i=0;i<slides.length;i++){
+		slides[i].style.display="none";
+	}
+	for(let i=0;i<circles.length;i++){
+		circles[i].className=circles[i].className.replace("active","");
+	}
+	slides[slidePage-1].style.display="block";
+	circles[slidePage-1].classList.add("active");
+	return slidePage;
+}
+function plusSlides(n,current_slidepage){
+	return showSlides(current_slidepage+=n);
+	
+}
